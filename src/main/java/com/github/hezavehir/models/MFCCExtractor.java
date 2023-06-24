c509aa3580391dd85f49bbe6b8e1aa41dc877c30
Hezavehir <hezavehir@gmail.com>
2023-06-24 21:11:13 +0330

    Working On MFCCExtractor

--- src/main/java/com/github/hezavehir/models/MFCCExtractor.java
+++ src/main/java/com/github/hezavehir/models/MFCCExtractor.java
@@ -1,53 +1,57 @@
 package com.github.hezavehir.models;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import be.tarsos.dsp.AudioDispatcher;
 import be.tarsos.dsp.AudioEvent;
 import be.tarsos.dsp.AudioProcessor;
 import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
-
-/*
- * This is a modified version of a code from: @lichard49
- * repository: lichard49/MFCCTest.java
- * url: https://gist.github.com/lichard49/69ab2a2ddd3c11394974c404ca102a55
- *
- * I'm sorry; I don't know if there is a better way to show
- * the owner of the code when I am writing this repository.
-*/
+import be.tarsos.dsp.mfcc.MFCC;
+import javafx.util.Pair;
 
 public class MFCCExtractor {
-    public static void main(String[] args) throws Exception {
-        final AudioDispatcher dispatcher = AudioDispatcherFactory.fromFile(
-                new File("./aud/Persian/numbers/1(yek).wav"),
-                2400,
-                1200);
 
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException {
+        File file = new File("/home/pouya/Desktop/voice-recognition/aud/Persian/numbers/2(do).wav");
+        // AudioDispatcher dispatcher = AudioDispatcherFactory.fromFile(file, 2048,
+        // 1024);
+        // int numCoefficients = 13;
+
+        // MFCC mfcc = new MFCC(2048, 44100, numCoefficients, 20, 300, 22000);
+        // dispatcher.addAudioProcessor(mfcc);
+        // dispatcher.run();
+        // float[] mfccValues = mfcc.getMFCC();
+        // System.out.println(Arrays.toString(mfccValues));
+
+        List<Pair<Float, float[]>> mfccList = new ArrayList<>(300);
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromFile(file, 2048, 512);
+        MFCC mfcc = new MFCC(2048, 44100, 13, 20, 300, 22000);
+        dispatcher.addAudioProcessor(mfcc);
         dispatcher.addAudioProcessor(new AudioProcessor() {
 
             @Override
             public void processingFinished() {
+                for (int i = 0; i < mfccList.size(); i++) {
+                    System.out.print("\n$$$" + mfccList.get(i).getKey() + ": ");
+                    for (int j = 0; j < mfccList.get(i).getValue().length; j++) {
+                        System.out.print(mfccList.get(i).getValue()[j] + " ");
+                    }
+                }
             }
 
             @Override
             public boolean process(AudioEvent audioEvent) {
-                System.out.println("### Time = " + audioEvent.getTimeStamp());
-                MyMFCC mfcc = new MyMFCC();
-                float[] mfccs = mfcc.getMFCC(audioEvent.getFloatBuffer(),
-                        audioEvent.getSampleRate(),
-                        13);
-                printArray("mfccs", mfccs);
+                mfccList.add(new Pair(audioEvent.getTimeStamp(), mfcc.getMFCC()));
                 return true;
             }
         });
+
         dispatcher.run();
     }
 
-    public static void printArray(String name, float[] array) {
-        System.out.println(name + " [" + array.length + "]");
-        for (float f : array) {
-            System.out.print(f + " ");
-        }
-        System.out.println('\n');
-    }
 }
